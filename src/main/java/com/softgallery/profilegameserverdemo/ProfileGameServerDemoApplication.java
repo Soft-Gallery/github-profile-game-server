28e854ed7ae33a769ddf12d4c1f85568eeae6edc
Seonho Kim <5061ksh@naver.com>
2024-03-14 16:55:38 +0900

    initialize

---
+++ src/main/java/com/softgallery/profilegameserverdemo/ProfileGameServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.softgallery.profilegameserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileGameServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileGameServerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softgallery/profilegameserverdemo/ProfileGameServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softgallery.profilegameserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileGameServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
